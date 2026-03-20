2aee892005fa561c1a3dcb94954a5d139620be7b
Matthew McCorkell <McCorkell-M@ulster.ac.uk>
2026-03-20 16:18:42 +0000

    main menu

--- Assets/MattsStuff/Code/AddOutline.cs
+++ Assets/MattsStuff/Code/AddOutline.cs
@@ -4,7 +4,7 @@ public class AddOutline : MonoBehaviour
 {
     private Outline Outline;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
             this.gameObject.AddComponent<Outline>();
    }
