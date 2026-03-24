3b0d8db335eed4a08b42a53f182f2e0b47542021
Matthew McCorkell <McCorkell-M@ulster.ac.uk>
2026-03-24 16:11:40 +0000

    lost most work
    im crying

--- Assets/CC'sStuff/Scripts/MainMenu.cs
+++ Assets/CC'sStuff/Scripts/MainMenu.cs
@@ -6,6 +6,55 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     private bool tutorialPlayed = false;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject CharacterObj;
+    [SerializeField] private GameObject BooksObj;
+
+    private void Start()
+    {
+        PlayCharacter();
+    }
+
+    public void PlayBooks()
+    {
+        HideCharacter();
+        StartCoroutine(LerpCamera(105));
+        Invoke(nameof(EnableBooks), 1.5f);
+    }
+
+    private IEnumerator LerpCamera(float xToRotate)
+    {
+        Debug.Log("Starting LerpCamera with target X rotation: " + xToRotate);
+        while (Mathf.Abs(mainCamera.transform.rotation.eulerAngles.x - xToRotate) > 0.1f)
+        {
+            mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, Quaternion.Euler(xToRotate, 0, 0), Time.deltaTime * 2);
+            Debug.Log(mainCamera.transform.rotation.eulerAngles.x);
+        }
+        yield return null;
+    }
+    private void EnableBooks()
+    {
+        BooksObj.SetActive(true);
+    }
+    private void HideBooks()
+    {
+        BooksObj.SetActive(false);
+    }
+
+    public void PlayCharacter()
+    {
+        HideBooks();
+        StartCoroutine(LerpCamera(55));
+        Invoke(nameof(EnableCharacter), 1.5f);
+    }
+    private void EnableCharacter()
+    {
+        CharacterObj.SetActive(true);
+    }
+    private void HideCharacter()
+    {
+        CharacterObj.SetActive(false);
+    }
 
 
     public void PlayMenu()
@@ -15,7 +64,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayTutorial()
     {
-        Debug.Log("Playing Tutorial :"+ SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Playing Tutorial :" + SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         tutorialPlayed = true;
     }
